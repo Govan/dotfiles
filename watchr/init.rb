2d9045931d0af1f437c1c05ad3c4112d45bde507
Gavin Montague <gavin@leftbrained.co.uk>
2013-01-27 16:15:09 +0000

    removed watchr

--- watchr/init.rb
+++
@@ -1,5 +0,0 @@
-# Ultimately, we'd have a neater structure here
-# to determine what we're watching and what our 
-# formatter/output should be
-
-puts  File.expand_path(File.join(__FILE__, "../rspec.general.rb"))
