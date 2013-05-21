39758afb541a1ae192b802420d0c6293c92eea21
Gavin Montague <gavin@leftbrained.co.uk>
2013-05-21 18:52:51 +0100

    and back to normal

--- install.rb
+++ install.rb
@@ -1,3 +1,61 @@
 #!/usr/bin/env ruby
 
-system %Q{touch foo}
+require 'erb'
+
+def install
+  replace_all = !!ENV["FORCE"]
+  Dir['*'].each do |file|
+    next if %w[install.rb README README.rdoc LICENSE].include? file
+
+    if File.exist?(File.join(ENV['HOME'], ".#{file.sub('.erb', '')}"))
+      if replace_all
+        replace_file(file)
+      else
+        print "overwrite ~/.#{file.sub('.erb', '')}? [ynaq] "
+        case $stdin.gets.chomp
+        when 'a'
+          replace_all = true
+          replace_file(file)
+        when 'y'
+          replace_file(file)
+        when 'q'
+          return 0
+        else
+          puts "skipping ~/.#{file.sub('.erb', '')}"
+        end
+      end
+    else
+      link_file(file)
+    end
+  end
+  touch_zshrc_local
+end
+
+def touch_zshrc_local
+  target = File.expand_path("~/.zshrc_local")
+  unless File.exists? target
+    File.open(target, 'w') do |new_file|
+      new_file << "# Put private customizations in here\n"
+      new_file << "# and/or link to one of the per-machine shared configs\n"
+    end
+  end
+end
+
+def replace_file(file)
+  system %Q{rm -rf "$HOME/.#{file.sub('.erb', '')}"}
+  link_file(file)
+end
+
+def link_file(file)
+  if file =~ /.erb$/
+    puts "generating ~/.#{file.sub('.erb', '')}"
+    File.open(File.join(ENV['HOME'], ".#{file.sub('.erb', '')}"), 'w') do |new_file|
+      new_file.write ERB.new(File.read(file)).result(binding)
+    end
+  else
+    puts "linking ~/.#{file}"
+    system %Q{ln -s "$PWD/#{file}" "$HOME/.#{file}"}
+  end
+end
+
+install
