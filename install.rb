fb3f3546fb00ad8c83d28c833c984c11e5471b9c
Gavin Montague <gavin@leftbrained.co.uk>
2014-03-09 21:24:32 +0000

    symlink files rather than replacing them and don't bother with the replace-all? option

--- install.rb
+++ install.rb
@@ -3,32 +3,23 @@
 require 'erb'
 
 def install
-  replace_all = !!ENV["FORCE"]
   Dir['*'].each do |file|
     next if %w[install.rb README README.rdoc LICENSE].include? file
-
-    if File.exist?(File.join(ENV['HOME'], ".#{file.sub('.erb', '')}"))
-      if replace_all
-        replace_file(file)
-      else
-        print "overwrite ~/.#{file.sub('.erb', '')}? [ynaq] "
-        case $stdin.gets.chomp
-        when 'a'
-          replace_all = true
-          replace_file(file)
-        when 'y'
-          replace_file(file)
-        when 'q'
-          return 0
-        else
-          puts "skipping ~/.#{file.sub('.erb', '')}"
-        end
-      end
-    else
-      link_file(file)
-    end
+    link_or_replace_file file
   end
+
   touch_zshrc_local
+end
+
+def link_or_replace_file(file)
+  if file =~ /.erb$/
+    puts "generating ~/.#{file.sub('.erb', '')}"
+    File.open(File.join(ENV['HOME'], ".#{file.sub('.erb', '')}"), 'w') do |new_file|
+      new_file.write ERB.new(File.read(file)).result(binding)
+    end
+  else
+    system %Q{ln -shFfv "$PWD/#{file}" "$HOME/.#{file}"}
+  end
 end
 
 def touch_zshrc_local
@@ -38,23 +29,6 @@ def touch_zshrc_local
       new_file << "# Put private customizations in here\n"
       new_file << "# and/or link to one of the per-machine shared configs\n"
     end
-  end
-end
-
-def replace_file(file)
-  system %Q{rm -rf "$HOME/.#{file.sub('.erb', '')}"}
-  link_file(file)
-end
-
-def link_file(file)
-  if file =~ /.erb$/
-    puts "generating ~/.#{file.sub('.erb', '')}"
-    File.open(File.join(ENV['HOME'], ".#{file.sub('.erb', '')}"), 'w') do |new_file|
-      new_file.write ERB.new(File.read(file)).result(binding)
-    end
-  else
-    puts "linking ~/.#{file}"
-    system %Q{ln -s "$PWD/#{file}" "$HOME/.#{file}"}
   end
 end
 
