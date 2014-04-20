8febc08665f2a34bd69c4e459cb9b3de14afc031
Gavin Montague <gavin@leftbrained.co.uk>
2014-04-20 21:20:15 +0100

    jk to swap modes, or not
    jk should be remappable to mode-swap in zsh-vim-mode. This isn't working for me though.

--- zsh/vim_mode.zsh
+++ zsh/vim_mode.zsh
@@ -1,6 +1,7 @@
-# Original Source http://dougblack.io/words/zsh-vi-mode.html
 bindkey -v
 
+####################################################
+# Keybindings was cargo-culted
 bindkey '^P' up-history
 bindkey '^N' down-history
 bindkey '^?' backward-delete-char
@@ -8,7 +9,14 @@ bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 bindkey '^r' history-incremental-search-backward
 
-# show vim status
+####################################################
+# Use jk to exit insert mode.
+# This does not work, and I'm buggered if I can work out why
+bindkey -M viins 'jk' vi-cmd-mode
+
+
+####################################################
+# show vim status as an [X] on the PROMPT
 function zle-line-init zle-keymap-select {
     VIM_PROMPT="${${KEYMAP/vicmd/X}/(main|viins)/ }"
     PROMPT="%{$BLUE%}%m [%{$RED%}${VIM_PROMPT}%{$BLUE%}]: %{$RESET%}";
@@ -18,6 +26,8 @@ function zle-line-init zle-keymap-select {
 zle -N zle-line-init
 zle -N zle-keymap-select
 
+#################################################
+# reduce the lag when swapping modes
 export KEYTIMEOUT=1
 
 # Extension to enable vim-like text-objects
