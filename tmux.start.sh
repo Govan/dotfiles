d3d69b18da1572737bde53e575c96621b7d4dfa8
Gavin Montague <gavin@leftbrained.co.uk>
2014-12-18 13:41:27 +0000

    Let's try forcing a decision about tmux for every new shell

---
+++ tmux.start.sh
@@ -0,0 +1,34 @@
+#!/bin/sh 
+# Taken from 
+# http://www.huyng.com/posts/productivity-boost-with-tmux-iterm2-workspaces/
+export PATH=$PATH:/usr/local/bin
+
+# abort if we're already inside a TMUX session
+[ "$TMUX" == "" ] || exit 0 
+
+# startup a "default" session if none currently exists
+tmux has-session -t _default || tmux new-session -s _default -d
+
+# present menu for user to choose which workspace to open
+PS3="Please choose your session: "
+options=($(tmux list-sessions -F "#S") "New Session" "Zsh")
+echo "Available sessions"
+echo "------------------"
+echo " "
+select opt in "${options[@]}"
+do
+    case $opt in
+        "New Session")
+            read -p "Enter new session name: " SESSION_NAME
+            tmux new -s "$SESSION_NAME"
+            break
+            ;;
+        "Zsh")
+            zsh --login
+            break;;
+        *) 
+            tmux attach-session -t $opt 
+            break
+            ;; 
+    esac
+done    
