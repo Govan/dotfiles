1a980b7cc65a34c10f32b3849372e854117d0198
Gavin Montague <gavin@leftbrained.co.uk>
2015-08-20 22:48:36 +0100

    Replace Ruby installer with a Shellscript
    The Problem:
    ============
    Installation required Ruby, which was a pain on servers.
    
    The Solution:
    =============
    Install with a pure Shellscript,

---
+++ install.sh
@@ -0,0 +1,44 @@
+
+create_zshrc_local() {
+  local zshrc_local="$HOME/.zshrc_local";
+  if [ ! -f $zshrc_local ]
+  then
+    touch $zshrc_local;
+    echo "Creating placeholder at ${zshrc_local}"
+    echo "# Put private customizations in here" >> $zshrc_local
+    echo "# and/or link to one of the per-machine shared configs" >> $zshrc_local
+  fi
+
+}
+
+link_dotfile() {
+  local source=$1
+  local filename=$(basename "$source");
+  if [ $filename == "install.sh" -o $filename == "README.md" ] 
+  then
+    return
+  else
+    ln -nsFfv "$source" "$HOME/.${filename}"
+  fi
+}
+
+link_dotfiles() {
+  local source_directory=$(dirname "$(cd "$(dirname "$0")" && pwd)/$(basename "$0")")
+  for file in $source_directory/*
+  do 
+    link_dotfile $file
+  done
+}
+
+install_git_submodules() {
+  git submodule init
+  git submodule update
+}
+
+main() {
+  install_git_submodules
+  create_zshrc_local
+  link_dotfiles
+} 
+
+main
